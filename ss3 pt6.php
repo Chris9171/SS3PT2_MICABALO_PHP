c7c6c5159851e0108d1826253a0eb3f96e5bf403
Chris9171 <micabalo.christianjosh@ici.edu.ph>
2025-04-03 06:42:12 +0800

    Update ss3 pt6.php

--- ss3 pt6.php
+++ ss3 pt6.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Grade Calculator</title>
+    <title>- Item Order </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style type="text/css">
